d71fab446b5edd9c205344d4554bc6544916243e
yaeoni <tjghwnd103@gmail.com>
2021-11-16 03:22:15 +0900

    [FEAT] TokenProvider 구현

---
+++ src/main/java/com/crewspace/auth/jwt/TokenProvider.java
@@ -0,0 +1,67 @@
+package com.crewspace.auth.jwt;
+
+import com.crewspace.auth.dto.TokenDTO;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+
+    // at, rt 유효 기간 설정
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String BEARER_TYPE = "bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30min
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7days
+
+    private final Key key;
+
+    // jwt 생성 암호화 Key 값
+    public TokenProvider(@Value("${jwt.secret}") String secretKey){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Access Token 과 Refresh Token 을 생성
+    public TokenDTO generateTokenDTO(Authentication authentication){
+        // 권한 가져오기
+        String authorities = authentication.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(","));
+
+        Long now = (new Date()).getTime();
+
+        // AT 생성 -> 유저 역할, 이름
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        String accessToken = Jwts.builder()
+            .setSubject(authentication.getName())       // payload "sub": "name"
+            .claim(AUTHORITIES_KEY, authorities)        // payload "auth": "ROLE_USER"
+            .setExpiration(accessTokenExpiresIn)        // payload "exp": 1516239022 (예시)
+            .signWith(key, SignatureAlgorithm.HS512)    // header "alg": "HS512"
+            .compact();
+
+        // RT 생성 -> 단순 생성 날짜
+        String refreshToken = Jwts.builder()
+            .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact();
+
+        return TokenDTO.builder()
+            .grantType(BEARER_TYPE)
+            .accessToken(accessToken)
+            .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
+            .refreshToken(refreshToken)
+            .build();
+    }
+
+
+
+}
