7ed1dbb561b06d304841cd4a629e6f1c87474494
yaeoni <tjghwnd103@gmail.com>
2021-11-18 00:29:55 +0900

    [ADD] Entity 추가(member, authority, basetime)

---
+++ src/main/java/com/crewspace/auth/entity/Member.java
@@ -0,0 +1,45 @@
+package com.crewspace.auth.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "member")
+public class Member extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String oauthId;
+
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public Member(Long id, String oAuthId, String email, Authority authority) {
+        this.id = id;
+        this.oauthId = oAuthId;
+        this.email = email;
+        this.authority = authority;
+    }
+
+    public Member update(String email){
+        this.email = email;
+        return this;
+    }
+}
