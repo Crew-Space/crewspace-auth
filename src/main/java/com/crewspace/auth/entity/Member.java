50b7f64ca17307a8cfa48dbf1773c3b05b014ebb
yaeoni <tjghwnd103@gmail.com>
2021-12-01 04:30:24 +0900

    [FIX] dummy와 실 카카오 로그인 유저 추가에 따른 duplicate 에러 해결

--- src/main/java/com/crewspace/auth/entity/Member.java
+++ src/main/java/com/crewspace/auth/entity/Member.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Builder;
@@ -17,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "member")
 public class Member extends BaseTimeEntity {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
