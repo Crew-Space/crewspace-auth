9fd0314ebb3d4bf45997fb4c5b281cc5e27dab15
yaeoni <tjghwnd103@gmail.com>
2021-12-01 04:29:55 +0900

    [ADD] 환경파일 추가 / 압축파일 경로 수정 / deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ubuntu/auth/deploy.log
 CURRENT_PID=$(sudo docker container ls -q)
+
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인 $CURRENT_PID" >> /home/ubuntu/auth/deploy.log
 
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/auth/deploy.log
 else
   echo "> sudo docker stop $CURRENT_PID"
-  sudo docker stop $CURRENT_PID
   sudo docker rm $CURRENT_PID
   sleep 5
 fi
